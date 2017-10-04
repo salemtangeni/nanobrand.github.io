c21c9aa29f42e8deec6810e15abb90ca24fdd413
hailongasalem <tangenisalem@outlook.com>
2017-10-04 14:26:50 +0200

    NANOBRAND GROUP SITE

---
+++ NavArray.php
@@ -0,0 +1,25 @@
+ <?php
+
+	$nav	=	array	(	array('label'=>'Home',
+								  'url'	=> 'home'),
+
+
+						    array('label' =>'Services',
+							      'url' => 'services'),
+
+
+						    array('label' =>'Products',
+							      'url' => 'products'),
+
+
+						    array('label' =>'About_Us',
+							      'url' => 'about_us'),
+
+
+						    array('label' =>'Contact_Us',
+							      'url' => 'contact_us'),
+										
+							array('label' => 'About_Developer',
+										'url' => 'about_developer')
+						);
+?>
---
+++ Pages/home.php
@@ -0,0 +1,99 @@
+<title>NANO BRAND (PTY) LTD</title>
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="R/css/bootstrap.min.css">
+	<link rel="stylesheet" href="R/css/font-awesome.min.css">
+
+	<!-- Custom styles-->
+	<link rel="stylesheet" href="R/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="R/css/main.css">
+</head>
+
+<body class="home">
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+				<a class="navbar-brand" href="index.html"><img src="R/images/Brand.png" alt="Nano Brand (pty) ltd"></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li class="active"><a href="#">HOME</a></li>
+					<li><a href="Products.html">PRODUCTS</a></li>
+					<li><a href="Staff.html">OUR TEAM</a></li>
+					<li><a href="about.html">ABOUT US</a></li>
+					<li><a href="contact.html">CONTACT US</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div> 
+	<!-- /.navbar -->
+
+	<!-- Header -->
+	<header id="head">
+		<div class="container">
+			<div class="row">
+				<h1 class="lead">WELCOME TO NANO BRAND (PTY) LTD.</h1>
+				<p class="tagline" style="color:Blue">A fully Namibian software company owned by indeginous Namibians</br>
+				                   that specialises and focuses on Website Development, Web Applications</br>
+								   and Desktop Software/Applications.</p>
+								   <style>
+								   .tagline
+								   {
+								   color: Balck;
+								   }
+								   </style>
+				<p><a class="btn btn-primary btn-lg" role="button">SEE OUR SERVICES</a> <a class="btn btn-info btn-lg" role="button">CONTACT US NOW</a></p>
+			</div>
+		</div>
+	</header>
+	<!-- /Header -->
+
+	<!-- Intro -->
+	<div class="container text-center">
+		<br> <br>
+		<h2 class="thin">The best place to tell people why they are here</h2>
+		<p class="text-muted">
+			The difference between involvement and commitment is like an eggs-and-ham breakfast:<br> 
+			the chicken was involved; the pig was committed.
+		</p>
+	</div>
+	<!-- /Intro-->
+		
+	<!-- Highlights - jumbotron -->
+	<div class="jumbotron top-space">
+		<div class="container">
+			
+			<h3 class="text-center thin">Why Pick us for your system?</h3>
+			
+			<div class="row">
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fa fa-cogs fa-5"></i>System build from scratch to suite your requirements</h4></div>
+					<div class="h-body text-center">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aliquid adipisci aspernatur. Soluta quisquam dignissimos earum quasi voluptate. Amet, dignissimos, tenetur vitae dolor quam iusto assumenda hic reprehenderit?</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fa fa-flash fa-5"></i>Fast, compatible and responsive</h4></div>
+					<div class="h-body text-center">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, commodi, sequi quis ad fugit omnis cumque a libero error nesciunt molestiae repellat quos perferendis numquam quibusdam rerum repellendus laboriosam reprehenderit! </p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fa fa-heart fa-5"></i>We love what we do. what you see is what you get</h4></div>
+					<div class="h-body text-center">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, vitae, perferendis, perspiciatis nobis voluptate quod illum soluta minima ipsam ratione quia numquam eveniet eum reprehenderit dolorem dicta nesciunt corporis?</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fa fa-smile-o fa-5"></i>We offer 2 Months Free maintance and support</h4></div>
+					<div class="h-body text-center">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, excepturi, maiores, dolorem quasi reprehenderit illo accusamus nulla minima repudiandae quas ducimus reiciendis odio sequi atque temporibus facere corporis eos expedita? </p>
+					</div>
+				</div>
+			</div> <!-- /row  -->
+		
+		</div>
+	</div>
+	<!-- /Highlights -->
